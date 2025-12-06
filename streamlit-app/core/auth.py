1f318cc12a6e571c90598f4b3df97bddf4499b5b
sasaxang <sasan.zangeneh@gmail.com>
2025-12-06 18:31:46 +0100

    Revert st.login code to fix generic auth config

--- streamlit-app/core/auth.py
+++ streamlit-app/core/auth.py
@@ -139,7 +139,7 @@ def show_auth_ui(lang="fa"):
                 use_container_width=True,
                 type="primary"
             ):
-                st.login("google")
+                st.login()
         
         return False
 
